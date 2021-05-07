c3f0c6f62c63c9896c49ac8e8de363157dd26736
Andrew Masih <>
2021-05-07 16:04:23 +0000

    added comments

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let i = 0;
 do {
-    console.log("I am happy!");
-    i++;
+  console.log("I am happy!");
+  i++;
 } while (i < 10);
+
+/* it runs the same code over and over again until the condition is met,the loop has 2 keywords: 'do' and 'while' */
+
+
+/* do {block code} while (condition): */
+
+/* the do while loop will run the block of code at least once, even though the condition is false, it means that the block of code runs before the condition is checked, the condition comes after the block of code; this loo is used if you want to run the block of code t least once no matter what*/
+
+/* the 'do while' loop will stop iteration when the condidto is false */
